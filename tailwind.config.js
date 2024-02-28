58e7559912a2fc32c06f69d02243f4137e004aac
Melon West <i.melonpub@gmail.com>
2024-02-28 14:04:45 +0600

    UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    fontSize:{
+      'xl': ['22px', '32px'],
+      '6xl': ['56px', '56px'],
+      '3xl':['32px', '40px']
+    },
+
+    extend: {
+      colors:{
+        'brown1':'#2E2829',
+        'brown2':'rgba(121, 110, 103, 0.20)',
+        'brown3': 'rgba(121, 110, 103, 0.50)',
+        'brown4':'#ECA153',
+        'brown5':'#9F815D'
+      }
+    },
   },
   plugins: [],
 }
